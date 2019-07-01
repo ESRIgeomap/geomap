43219c9e04f49c39a4b0a595e95d6a48a43d42da
lihubadboy <lihubadboy@163.com>
2019-07-01 09:27:53 +0800

    提交

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import { createMapView } from './middleware/arcgis/mapview/arcgis-mapview';
 import { bookmarks } from './middleware/arcgis/mapview/arcgis-bookmark';
 import { search } from './middleware/arcgis/mapview/arcgis-search';
-// import { createSceneView } from './middleware/arcgis/sceneview/arcgis-sceneview';
+import { createSceneView } from './middleware/arcgis/sceneview/arcgis-sceneview';
 import { createPopup } from './middleware/arcgis/mapview/arcgis-popup';
 import { toolbar } from './middleware/arcgis/sceneview/arcgis-toolbar';
 import { weatherEffects } from './middleware/arcgis/sceneview/arcgis-weatherEffects'; // 天气特效 中间件
@@ -16,7 +16,7 @@ export const dva = {
   config: {
     onAction: [
       // arcgisAuth(),
-      // createSceneView(),
+      createSceneView(),
       createMapView(),
       createPopup(),
       toolbar(),
