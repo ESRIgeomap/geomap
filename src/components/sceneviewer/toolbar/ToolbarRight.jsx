2c9fa7a4d7e1f5fc6292f1f7ad47f053cfa468da
dengdong <dengd@esrichina.com.cn>
2019-07-05 10:07:15 +0800

    删除sceneViewer下不用的组件

--- src/components/sceneviewer/toolbar/ToolbarRight.jsx
+++ src/components/sceneviewer/toolbar/ToolbarRight.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ModifyDataSource from '../ModifyDataSource/index';
-import SearchBtn from '../SearchBtn/index';
-import SelectionTool from '../SelectionTool/index';
 import BaseMap from '../BaseMap/index';
 import LightShadow from '../LightShadow/index';
 import Measure3Dbutton from '../Measure3Dbutton/index';
@@ -12,10 +10,6 @@ const ToolbarRight = () => {
     <div className={styles['toolbar-right']}>
       {/* 修改数据源 */}
       <ModifyDataSource />
-      {/* 搜索 */}
-      <SearchBtn />
-      {/* 框选 */}
-      <SelectionTool />
       {/* 底图切换 */}
       <BaseMap />
       {/* 光照阴影 */}
