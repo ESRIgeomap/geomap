0961edb02e7a4d24807c12361247d8b925304312
lihubadboy <lihubadboy@163.com>
2019-07-05 17:03:14 +0800

    更新图标

--- src/components/sceneviewer/toolbar/ToolbarRight.jsx
+++ src/components/sceneviewer/toolbar/ToolbarRight.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ModifyDataSource from '../ModifyDataSource/index';
 import BaseMap from '../BaseMap/index';
+import Layerlist from '../Layerlist/index';
 import LightShadow from '../LightShadow/index';
 import Measure3Dbutton from '../Measure3Dbutton/index';
 import styles from './ToolbarRight.css';
@@ -10,11 +11,13 @@ const ToolbarRight = () => {
     <div className={styles['toolbar-right']}>
       {/* 修改数据源 */}
       <ModifyDataSource />
+      {/* 图层列表 */}
+      <Layerlist />
       {/* 底图切换 */}
       <BaseMap />
       {/* 光照阴影 */}
       <LightShadow />
-
+      {/* 三维分析 */}
       <Measure3Dbutton />
     </div>
   );
